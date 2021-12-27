aff7cd105576359636d2fa37797194b80d4753bf
Ahn ByungUk <byunguk.ahn@innowireless.com>
2021-12-27 17:00:38 +0900

    [ch11] Remove Dollar, Franc class
     - remove dollar, franc class because Money integrate both classes

--- Money/src/test/java/MoneyTest.java
+++ Money/src/test/java/MoneyTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.*;
         Equal null
         Equal object
         hasCode()
-        FrancMultiplicationTest 지워야하나?
         Dollar/Franc 중복
     COMPLETE:
         amount를 private로
@@ -24,22 +23,12 @@ import static org.junit.jupiter.api.Assertions.*;
         Franc과 Dollar 비교
         통화?
         공용 times
+        FrancMultiplicationTest 지워야하나?
+
 
 
  */
 class MoneyTest {
-
-    @Test
-    void multiplicationWithSideEffectTest() {
-        //$5 * 2 = $10
-        //Dollar의 부작용
-        Dollar five = new Dollar(5);
-        five.timesDirectToAmount(2);
-        assertThat(10, is(five.amount));
-        five.timesDirectToAmount(3);
-        assertThat(15, is(not(five.amount)));
-    }
-
     @Test
     void multiplicationTest() {
         Money five = Money.dollar(5);
@@ -52,29 +41,14 @@ class MoneyTest {
         //equals
         assertThat(Money.dollar(5), is(Money.dollar(5)));
         assertThat(Money.dollar(5), is(not(Money.dollar(6))));
-        assertThat(Money.franc(5), is(Money.franc(5)));
-        assertThat(Money.franc(5), is(not(Money.franc(6))));
 
         //equals to different currency
         assertThat(Money.franc(5), is(not(Money.dollar(5))));
     }
 
     @Test
-    void francMultiplicationTest() {
-        Money five = Money.franc(5);
-        assertThat(Money.franc(10), is(five.times(2)));
-        assertThat(Money.franc(15), is(five.times(3)));
-    }
-
-    @Test
     void currencyTest() {
         assertThat(Money.dollar(1).currency(), is("USD"));
         assertThat(Money.franc(1).currency(), is("CHF"));
-    }
-
-    @Test
-    void differentClassEquality() {
-        assertThat(new Money(10, "USD"), is(new Dollar(10, "USD")));
-        assertThat(new Money(10, "CHF"), is(new Franc(10, "CHF")));
     }
 }
